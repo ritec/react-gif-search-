684fd31c2cb49465a4a5700e8d1ad5037b4e7a3e
Ri Caragol <ri.caragol@localstake.com>
2016-09-11 23:20:21 -0400

    added modal windows onClick for gifs

--- src/components/GifItem.js
+++ src/components/GifItem.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const GifItem = (image) => {
+const GifItem = ({gif, onGifSelect}) => {
   return (
-    <div className="gif-item">
-      <img src={image.gif.images.downsized.url} />
+    <div className="gif-item" onClick={() => onGifSelect(gif)}>
+      <img src={gif.images.downsized.url} />
     </div>
-  );
+  )
 };
 
 export default GifItem;
