88e0a67e28164dd236c2204eb83518374b7ecc87
Ri Caragol <ri.caragol@localstake.com>
2016-09-11 22:09:46 -0400

    working gif example

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GifList from './components/GifList';
 import SearchBar from './components/SearchBar';
+import request from 'superagent';
 
 
 class App extends React.Component {
@@ -9,30 +10,21 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      gifs: [
-        {
-          id: 1,
-          url: 'http://fakeimg.pl/300/'
-        },
-        {
-          id: 2,
-          url: 'http://fakeimg.pl/300/'
-        },
-        {
-          id: 3,
-          url: 'http://fakeimg.pl/300/'
-        }
-      ]
+      gifs: []
     }
   }
 
   handleTermChange(term) {
-    console.log(term);
+    const url = `http://api.giphy.com/v1/gifs/search?q=${term.replace(/\s/g, '+')}&api_key=dc6zaTOxFJmzC`;
+    request.get(url, (err, res) => {
+      this.setState({ gifs: res.body.data })
+    });
   }
+
   render() {
     return (
       <div>
-        <SearchBar onTermChange={this.handleTermChange} />
+        <SearchBar onTermChange={term => this.handleTermChange(term)} />
         <GifList gifs={this.state.gifs} />
       </div>
     );
